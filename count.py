9a3faef69417b9d0e876497511aafa0ab3af24b7
amakarudze <amakarudze@gmail.com>
2018-11-06 21:39:10 +0200

    First commit

---
+++ count.py
@@ -0,0 +1,9 @@
+"""Count how many times a word is repeated in a sentence."""
+
+
+def count_words(sentence):
+    words = sentence.lower().split(" ")
+    word_dict = {}
+    for word in words:
+        word_dict[word] = words.count(word)
+    return word_dict

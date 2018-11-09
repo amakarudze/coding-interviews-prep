68ff1f0fb780c55b9d80987709f2495e4d0a0b3a
amakarudze <amakarudze@gmail.com>
2018-11-09 21:10:17 +0200

    Modify permutations to accept non-dictionary words

--- test_permutations.py
+++ test_permutations.py
@@ -6,17 +6,17 @@ from itertools import permutations
 def perms(word):
     is_palindrome = False
     for w in permutations(word.lower()):
-        if w == w[::-1]:
+        if ''.join(w).strip(' ') == ''.join(w[::-1]).strip(' '):
             is_palindrome = True
+            break
         else:
             is_palindrome = False
-        print(w)
     return is_palindrome
 
 
 def test_permutations():
     assert perms('anna') == True
-    assert perms('Tact Coa') == False
+    assert perms('Tact Coa') == True
     assert perms('hear') == False
 
 
